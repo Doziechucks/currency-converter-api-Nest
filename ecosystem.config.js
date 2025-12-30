695ce583cf4724ec1e7845756bb7f2eaf0180235
Doziechucks <chiedoziechukwukam@gmail.com>
2025-12-30 17:50:50 +0100

    correct work flow no loop

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  apps: [
-    {
-      name: 'currency-api',
-      script: './dist/main.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '800M',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3000,
-      },
+  apps: [{
+    name: 'currency-converter',
+    script: './dist/main.js',
+    instances: 1,
+    exec_mode: 'fork',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
     },
-  ],
+    min_uptime: '10s',
+    max_restarts: 10,
+    restart_delay: 4000
+  }]
 };
 
